e9191f350e27f929ef8aba6de3c8a7f9745cf30d
JoaquinFernnadez <joaquinfernandezcarballo@gmail.com>
2025-01-17 09:40:21 +0100

    First Commit

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express, {Response, Request} from "express"
+const app = express()
+
+app.get('/', (req:Request, res:Response) => {
+    res.send('Bienvenido al backend')
+})
+
+export default app 
