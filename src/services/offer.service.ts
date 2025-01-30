f8547c1a55d02b8303b065a8b45cc3cde7005d23
JoaquinFernnadez <joaquinfernandezcarballo@gmail.com>
2025-01-30 10:06:15 +0100

    offer acabado

--- src/services/offer.service.ts
+++ src/services/offer.service.ts
@@ -1,40 +1,54 @@
 import HttpException from "../exceptions/HttpException";
-import { Offer, PrismaClient } from "@prisma/client";
+import { Offer, PrismaClient, User } from "@prisma/client";
 
 const prisma = new PrismaClient()
 
 export class OfferService {
 
-    static async getById(id:number){
-        const findUser =  await prisma.user.findUnique({ where: {id}})
-        if(!findUser)  throw new HttpException (404, 'User not found')
+    static async getById(id: number) {
+        const findUser = await prisma.user.findUnique({ where: { id } })
+        if (!findUser) throw new HttpException(404, 'User not found')
         return findUser
     }
-    static async getAll(){
+    static async getAll() {
         const offers = await prisma.offer.findMany()
         return offers
     }
-    static async create(body: Offer){
-        const offers = await prisma.offer.create({data:{
-            ...body}})
+    static async create(body: Offer) {
+        const offers = await prisma.offer.create({
+            data: {
+                ...body
+            }
+        })
         return offers
     }
-    static async delete(id: number){
-        const offer = await prisma.offer.delete({where:{id}})
-        if(!offer)  throw new HttpException (404, 'Offer not found')
+    static async delete(id: number) {
+        const offer = await prisma.offer.delete({ where: { id } })
+        if (!offer) throw new HttpException(404, 'Offer not found')
         return "offert deleted"
     }
-    static async update(){
+    static async update() {
         const users = await prisma.user.findMany()
         return users
     }
-    static async rate(){
-        const users = await prisma.user.findMany()
-        return users
+    static async rate(idOffer: number, rate: number, user: User) {
+        const findOffert = await prisma.offer.findUnique({ where: { id: idOffer } })
+        if (!findOffert) throw new HttpException(404, 'Offert not found')
+        return await prisma.rate.create({
+            data: {
+                value: rate,
+                idOffer: idOffer,
+                idUser: user.id
+            }
+        })
     }
-    static async getRate(){
-        const users = await prisma.user.findMany()
-        return users
+
+    static async getRate(idOffer: number) {
+        const findOffert = await prisma.offer.findUnique({ where: { id: idOffer } })
+        if (!findOffert) throw new HttpException(404, 'Offert not found')
+        const rate = await prisma.rate.findMany({ where: { idOffer } })
+        const sum = rate.reduce((acc, curr) => acc + curr.value, 0)
+        return sum / rate.length
     }
 
 
